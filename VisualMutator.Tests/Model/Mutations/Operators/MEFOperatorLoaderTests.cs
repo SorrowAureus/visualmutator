2a240704e67d6f95c01d7ffb67171e9698b49941
pavzaj <pavzaj@gmail.com>
2016-11-13 23:39:40 +0100

    Fixing Unit Tests

---
+++ VisualMutator.Tests/Model/Mutations/Operators/MEFOperatorLoaderTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace VisualMutator.Model.Mutations.Operators.Tests
+{
+    [TestFixture()]
+    public class MEFOperatorLoaderTests
+    {
+        [Test()]
+        public void ReloadOperatorsTest()
+        {
+            var subject = new MEFOperatorLoader();
+
+            var result = subject.ReloadOperators();
+        }
+    }
+}
