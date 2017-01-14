32526781ddb30a8d0cc11d8632a990211a3704ca
pavzaj <pavzaj@gmail.com>
2017-01-14 18:04:17 +0100

    Added basic Sonarqube integration fix

---
+++ RunOpencoverCodeCoverageSession.bat
@@ -0,0 +1,1 @@
+.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -mergebyhash -skipautoprops -register:user -output:opencoverTestCoverageReport.xml -target:".\packages\NUnit.ConsoleRunner.3.5.0\tools\nunit3-console.exe" -targetargs:"VisualMutator.sln"
