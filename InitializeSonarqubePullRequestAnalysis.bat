f156fda42f53c583820d5a36a01a359113996472
pavzaj <pavzaj@gmail.com>
2017-03-05 16:52:36 +0100

    PR analysis test

---
+++ InitializeSonarqubePullRequestAnalysis.bat
@@ -0,0 +1,3 @@
+echo %APPVEYOR_REPO_COMMIT%
+@echo off
+packages\MSBuild.SonarQube.Runner.Tool.1.0.0\tools\SonarQube.Scanner.MSBuild.exe begin /k:"Visualmutator-pavzaj" /d:"sonar.host.url=https://sonarqube.com" /v:"%APPVEYOR_REPO_COMMIT%" /d:"sonar.links.scm=https://github.com/pavzaj/visualmutator" /d:"sonar.cs.opencover.reportsPaths=OpencoverTestCoverageReport.xml" /d:sonar.cs.nunit.reportsPaths=nunit2TestResult.xml" /d:"sonar.login=%SONARQUBE_TOKEN%" /d:"sonar.analysis.mode=preview" /d:"sonar.github.pullRequest=%APPVEYOR_PULL_REQUEST_NUMBER%" /d:"sonar.github.oauth=%github_oauth_token%" /d:"sonar.github.repository=pavzaj/visualmutator"
